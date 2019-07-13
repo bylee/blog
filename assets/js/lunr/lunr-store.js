b4552810bd533b0e6731471bdcbc75a3097ee21d
bylee <bylee78@gmail.com>
2019-07-13 16:22:21 +0900

    Change baseurl

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -2,5 +2,5 @@ var store = [{
         "title": "블로그 시작하기",
         "excerpt":"글을 어디에 쓸까? 처음에는 미디엄이나 브런치에 쓰려고 했는데, 생각보다 자유도가 떨어진다는 것을 알고나서 아예 정적 페이지 생성기쪽으로 생각을 바꿜다. 요즘 뜨고 있는 게츠비를 고려했는데, 커스터마이징이나 문제 해결에 시간을 쓰다보면 주객인 전도될 것같아 지킬로 작성해서 깃헙에 올리기로 하였다. 지킬이란? 간단한 명령으로 정적 사이트를 생성할 수 있는 오픈 소스이다. 간단한 사용법과 많은...","categories": ["blog"],
         "tags": ["life","jekyll"],
-        "url": "https://bylee.github.io/blog/welcome-to-jekyll/",
+        "url": "https://bylee.github.io/blog/blog/welcome-to-jekyll/",
         "teaser":null}]
